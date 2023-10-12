55a2df5cdd514231d5c1e7dd09c08acc58012d1a
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-12 15:39:09 +0500

    Fixed load_dotenv problem with DJANGO_SETTINGS_MODULE

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,6 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE'))
 
