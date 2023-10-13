199c8c86e563afe74b067daf67efbdec2fa65cd4
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-13 13:50:33 +0500

    added create and list api endpoints for the orders app

--- core/generator.py
+++ core/generator.py
@@ -11,8 +11,9 @@ DJANGO_SETTINGS_MODULE = getenv('DJANGO_SETTINGS_MODULE')
 class HttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
-        if DJANGO_SETTINGS_MODULE == 'core.settings.development':
-            schema.schemes = ["http", "https"]
-        elif DJANGO_SETTINGS_MODULE == 'core.settings.production':
-            schema.schemes = ["https", "http"]
+        schema.schemes = ["https", "http"]
+        # if DJANGO_SETTINGS_MODULE == 'core.settings.development':
+        #     schema.schemes = ["http", "https"]
+        # elif DJANGO_SETTINGS_MODULE == 'core.settings.production':
+        #     schema.schemes = ["https", "http"]
         return schema
