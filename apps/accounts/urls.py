99bed96d95123ef36be74c3e09ce84668fa5b235
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-14 21:25:40 +0500

    added user/ prefix to User model APIs

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,15 +6,15 @@ from . import api_endpoints as views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.UserRegisterAPIView.as_view(), name='register'),
-    path('login/', views.UserLoginAPIView.as_view(), name='login'),
-    path('logout/', views.UserLogoutAPIView.as_view(), name='logout'),
-    path('retrieve/', views.UserRetrieveAPIView.as_view(), name='retrieve'),
-    path('update/', views.UserUpdateAPIView.as_view(), name='update'),
-    path('delete/', views.UserDeleteAPIView.as_view(), name='delete'),
-    path('check/', views.UserCheckAPIView.as_view(), name='check'),
-    path('token/', TokenObtainPairView.as_view(), name='token'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', views.UserRegisterAPIView.as_view(), name='register'),
+    path('user/login/', views.UserLoginAPIView.as_view(), name='login'),
+    path('user/logout/', views.UserLogoutAPIView.as_view(), name='logout'),
+    path('user/retrieve/', views.UserRetrieveAPIView.as_view(), name='retrieve'),
+    path('user/update/', views.UserUpdateAPIView.as_view(), name='update'),
+    path('user/delete/', views.UserDeleteAPIView.as_view(), name='delete'),
+    path('user/check/', views.UserCheckAPIView.as_view(), name='check'),
+    path('user/token/', TokenObtainPairView.as_view(), name='token'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     #########################################################################
     path('related_person/create/', views.RelatedPersonCreateAPIView.as_view(), name='related_person_create'),
     path('related_person/list/', views.RelatedPersonListAPIView.as_view(), name='related_person_list'),
