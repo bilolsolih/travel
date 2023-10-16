3f00779094894f363f20f3f37312973aa1793312
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-16 10:40:37 +0500

    registered the Region model into admin site

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Region
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id']
+    list_editable = ['title']
+    search_fields = ['title']
 # Register your models here.
