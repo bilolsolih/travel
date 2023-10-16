93a1098d6078acd69b3d67d548fcc4351e38f86f
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-16 10:27:01 +0500

    removed gender and district fields from UserUpdateSerializer

--- apps/accounts/api_endpoints/User/User_Update/serializers.py
+++ apps/accounts/api_endpoints/User/User_Update/serializers.py
@@ -7,6 +7,5 @@ class UserUpdateSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'email', 'first_name', 'last_name', 'middle_name', 'gender', 'birthdate',
-            'passport_picture', 'individual_pin', 'serial_number', 'region', 'district',
+            'email', 'first_name', 'last_name', 'middle_name', 'birthdate', 'passport_picture', 'individual_pin', 'serial_number', 'region'
         ]
