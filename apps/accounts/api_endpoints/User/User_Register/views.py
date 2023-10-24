00197bbe06bc5f29374c83b3130c40616644d2d7
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-24 15:26:02 +0500

    added payments app and two apis for it

--- apps/accounts/api_endpoints/User/User_Register/views.py
+++ apps/accounts/api_endpoints/User/User_Register/views.py
@@ -1,10 +1,12 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import MultiPartParser
 
 from .permissions import IsNotAuthenticated
 from .serializers import UserRegisterSerializer
 
 
 class UserRegisterAPIView(CreateAPIView):
+    parser_classes = [MultiPartParser]
     permission_classes = [IsNotAuthenticated]
     serializer_class = UserRegisterSerializer
 
