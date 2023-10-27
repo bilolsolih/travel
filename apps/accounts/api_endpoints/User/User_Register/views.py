fc58bbff528552d26a0de3a08d64851a6fea496f
bilolsolih <127647821+bilolsolih@users.noreply.github.com>
2023-10-26 22:43:09 -0700

    Update views.py

--- apps/accounts/api_endpoints/User/User_Register/views.py
+++ apps/accounts/api_endpoints/User/User_Register/views.py
@@ -27,24 +27,3 @@ class UserRegisterAPIView(CreateAPIView):
 
 
 __all__ = ['UserRegisterAPIView']
-from rest_framework_simplejwt.tokens import RefreshToken
-from apps.accounts.models import User, OTPCode
-
-
-class UserTokenObtainSerializer(serializers.Serializer):
-    phone_number = serializers.CharField()
-    code = serializers.CharField(max_length=4)
-
-    def validate(self, attrs):
-        try:
-            user = User.objects.get(phone_number=attrs['phone_number'])
-            exist = True
-        except User.DoesNotExist:
-            exist = False
-        #
-        #     try:
-        #         OTPCode.objects.get(user=user, code=attrs['code'], is_expired=False)
-        #     except OTPCode.DoesNotExist:
-        #         raise serializers.ValidationError({'code': 'Code for this user does not exist.'})
-        if attrs['code'] != '1111':
-            raise serializers.Validatio
