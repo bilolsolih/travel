db4e893adf588eaad86b2226475d97b78001d507
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-30 14:31:30 +0500

    added filter by title and popular places for PackageListAPIView

--- apps/packages/api_endpoints/Package/Package_List/views.py
+++ apps/packages/api_endpoints/Package/Package_List/views.py
@@ -1,12 +1,23 @@
+import django_filters
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from django_filters.rest_framework.filterset import FilterSet
 from rest_framework.generics import ListAPIView
 
 from apps.packages.models import Package
+from apps.places.models import PopularPlace
 from .serializers import PackageListSerializer
+
+
+class PackageFilterSet(FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    popular_places = django_filters.ModelChoiceFilter(field_name='popular_places', to_field_name='id', queryset=PopularPlace.objects.all())
 
 
 class PackageListAPIView(ListAPIView):
     serializer_class = PackageListSerializer
     queryset = Package.objects.filter(is_active=True)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PackageFilterSet
 
 
 __all__ = ['PackageListAPIView']
