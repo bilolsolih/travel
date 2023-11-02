80f53d0174bf27a122afe735c8df501e78b4b7af
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-11-02 09:31:33 +0500

    added discounts app

--- apps/packages/api_endpoints/Package/Package_List/views.py
+++ apps/packages/api_endpoints/Package/Package_List/views.py
@@ -22,7 +22,7 @@ class PackageListAPIView(ListAPIView):
     def get_queryset(self):
         queryset = Package.objects.filter(is_active=True)
         if self.request.query_params.get('discount', None):
-            queryset = queryset.filter(plans__discount__gt=0)
+            queryset = queryset.filter(discounts__pk=0).distinct()
         return queryset
 
 
