3e35d18f4d767e96a0df7c8ef8e108ccf27d03bd
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-11-03 03:10:33 -0700

    days field has been added to the package list serializer

--- apps/packages/api_endpoints/Package/Package_List/views.py
+++ apps/packages/api_endpoints/Package/Package_List/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework.backends import DjangoFilterBackend
 from django_filters.rest_framework.filterset import FilterSet
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
 
 from apps.packages.models import Package
 from apps.places.models import PopularPlace
@@ -16,14 +17,22 @@ class PackageFilterSet(FilterSet):
 
 class PackageListAPIView(ListAPIView):
     serializer_class = PackageListSerializer
+    pagination_class = LimitOffsetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = PackageFilterSet
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.query_params.get('nopage', None):
+            self.pagination_class = None
 
     def get_queryset(self):
         queryset = Package.objects.filter(is_active=True)
         discount = self.request.query_params.get('discount', None)
-        if discount is not None:
-            queryset = queryset.filter(discounts__pk=discount).distinct()
+        if discount:
+            if discount == 0:
+                queryset = queryset.fitlter(discounts__isnull=True)
+            else:
+                queryset = queryset.filter(discounts__pk=discount).distinct()
         return queryset
 
 
