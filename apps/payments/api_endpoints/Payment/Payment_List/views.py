fe8b68c9631afdb533b9b25097667cd5602abd6f
SolihCoder <BilolMuhammadSolih@gmail.com>
2023-11-22 11:55:49 +0500

    added ActivityRetrieveAPIView

--- apps/payments/api_endpoints/Payment/Payment_List/views.py
+++ apps/payments/api_endpoints/Payment/Payment_List/views.py
@@ -1,19 +1,22 @@
 import django_filters
+from django_filters.filterset import FilterSet
+from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from apps.payments.models import Payment
 from .serializers import PaymentListSerializer
-from django_filters.rest_framework.backends import DjangoFilterBackend
-from django_filters.filterset import FilterSet
 
 
 class PaymentFilterSet(FilterSet):
-    title = django_filters.CharFilter(field_name='package')
+    title = django_filters.CharFilter(field_name='order__package__title', lookup_expr='icontains')
+
 
 class PaymentListAPIView(ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PaymentListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PaymentFilterSet
 
     def get_queryset(self):
         return Payment.objects.filter(payer=self.request.user)
