55f2872d7d7df50c5bbbafc9dcfc24bd10ce11aa
SolihCoder <BilolMuhammadSolih@gmail.com>
2023-12-05 16:47:29 +0500

    added FQA and TermsAndConditions models

--- apps/about/signals.py
+++ apps/about/signals.py
@@ -2,7 +2,7 @@ from django.core.cache import cache
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import Location, SocialMedia
+from .models import Location, SocialMedia, FrequentlyAskedQuestion, TermsAndConditions
 
 
 @receiver(post_save, sender=Location)
@@ -15,3 +15,15 @@ def delete_cache(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=SocialMedia)
 def delete_cache(sender, instance, created, **kwargs):
     cache.delete('about:social_media')
+
+
+@receiver(post_save, sender=FrequentlyAskedQuestion)
+@receiver(post_delete, sender=FrequentlyAskedQuestion)
+def delete_cache(sender, instance, created, **kwargs):
+    cache.delete('about:questions')
+
+
+@receiver(post_save, sender=TermsAndConditions)
+@receiver(post_delete, sender=TermsAndConditions)
+def delete_cache(sender, instance, created, **kwargs):
+    cache.delete('about:terms')
