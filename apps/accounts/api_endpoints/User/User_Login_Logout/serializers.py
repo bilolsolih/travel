09fc345b0229d35127c46305f76eda6e7c78a1b1
SolihCoder <SolihCoder@gmail.com>
2025-01-26 19:24:51 +0500

    some changes, finally

--- apps/accounts/api_endpoints/User/User_Login_Logout/serializers.py
+++ apps/accounts/api_endpoints/User/User_Login_Logout/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class UserLoginSerializer(serializers.Serializer):
     login = serializers.CharField()
-    password = serializers.CharField(min_length=8, max_length=32)
+    password = serializers.CharField(min_length=4, max_length=32)
